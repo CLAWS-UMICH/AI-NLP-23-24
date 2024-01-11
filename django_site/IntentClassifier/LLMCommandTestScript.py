a5e95494e437c580f04e6d1cf8b5e9f643b940bf
Nathan Yap <nathancyap@gmail.com>
2024-01-10 20:35:24 -0500

    Test file

--- django_site/IntentClassifier/LLMCommandTestScript.py
+++ django_site/IntentClassifier/LLMCommandTestScript.py
@@ -9,10 +9,15 @@ def send_post_request(message=""):
     }
     return requests.post(TEST_ENDPOINT, json=request_obj)
 
-def run_test_case(message, expected_response):
+def run_test_case(message, expected_response, expected_data):
     response = send_post_request(message)
     print(response)
     return expected_response == respones.text
 
 if __name__ == '__main__':
-    run_test_case("TEST", "TEST")
+
+    run_test_case(
+        "create a geosample note which describes a red basaltic rock at location A", 
+        "[geosample]", 
+        {'color':'red', 'type':'basalt', 'location':'a'}
+    )
